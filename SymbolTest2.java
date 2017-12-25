f77aa20716c6b43c08a1f6f6d8489c755e74caf9
Barchenko1 <33847867+Barchenko1@users.noreply.github.com>
2017-12-26 01:04:23 +0200

    Delete SymbolTest2.java

--- SymbolTest2.java
+++
@@ -1,9 +0,0 @@
-public abstract class SymbolTest2 {
-    public static void main(String[] args) {
-        double num1 = Double.parseDouble( args[0] ) ;
-        double num2 = Double.parseDouble( args[1] ) ;
-        double gr=Math.atan(Double.parseDouble(String.valueOf(num1/num2))) ;
-        System.out.println("В радианах: "+gr);
-        System.out.println("В градусах: "+Math.toDegrees(gr));
-    }
-}
